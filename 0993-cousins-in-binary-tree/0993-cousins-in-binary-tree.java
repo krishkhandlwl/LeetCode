7bd12553b3f9c236f42f2d1b09cc713798560cd0
Krish handelwal <78268122+krishkhandlwl@users.noreply.github.com>
2023-02-05 21:37:36 +0530

    Update 0993-cousins-in-binary-tree.java

--- 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.java
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.java
@@ -13,6 +13,38 @@
  *     }
  * }
  */
+
+
+class Solution {
+    int xDepth =-1;
+    int yDepth = -2;
+    TreeNode xParent = null;
+    TreeNode yParent = null;
+
+    public boolean isCousins(TreeNode root, int x, int y) {
+        getHeightParent(root, x, y, null, 0);
+        return xDepth == yDepth && xParent != yParent;
+    }
+    public void getHeightParent(TreeNode node, int x, int y, TreeNode parent, int height){
+        
+        if(node == null) return;
+        if(node.val == x){
+            xDepth = height;
+            xParent = parent;
+        }
+        if(node.val == y){
+            yDepth = height;
+            yParent = parent;
+        }
+        parent = node;
+        
+        getHeightParent(node.left, x, y, parent, height+1);
+        getHeightParent(node.right, x, y, parent, height+1);
+        
+    }
+}
+
+/*
 class Solution {
     //BFS Approach
     public boolean isCousins(TreeNode root, int x, int y) {
@@ -43,3 +75,4 @@ class Solution {
         return xHeight == yHeight;
     }
 }
+*/
