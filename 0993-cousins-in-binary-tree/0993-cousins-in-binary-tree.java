effb9d7d3af40c78325988126b2d1f51a55b3fa8
Krish handelwal <78268122+krishkhandlwl@users.noreply.github.com>
2023-02-05 19:15:29 +0530

    Time: 1 ms (39.21%), Space: 40.1 MB (75.23%) - LeetHub

--- 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.java
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.java
@@ -14,30 +14,32 @@
  * }
  */
 class Solution {
-    int xDepth =-1;
-    int yDepth = -2;
-    TreeNode xParent = null;
-    TreeNode yParent = null;
-
+    //BFS Approach
     public boolean isCousins(TreeNode root, int x, int y) {
-        getHeightParent(root, x, y, null, 0);
-        return xDepth == yDepth && xParent != yParent;
-    }
-    public void getHeightParent(TreeNode node, int x, int y, TreeNode parent, int height){
-        
-        if(node == null) return;
-        if(node.val == x){
-            xDepth = height;
-            xParent = parent;
+        int xHeight = -1;
+        int yHeight = -2;
+        int height =0;
+        Queue<TreeNode> mainQ = new ArrayDeque<>();
+        mainQ.add(root);
+        while(mainQ.size() > 0){
+            int size = mainQ.size();
+            for(int i=0; i<size; i++){
+                TreeNode node = mainQ.remove();
+                if(node.left!= null && node.right != null){
+                if((node.left.val == x && node.right.val ==y) || (node.left.val == y && node.right.val == x))                   return false;
+                }
+                if(node.val == x){
+                    xHeight = height;
+                }
+                if(node.val == y){
+                    yHeight = height;
+                }
+                if(node.left!= null) { mainQ.add(node.left);  }
+                if(node.right!= null) { mainQ.add(node.right);  }    
+            }
+            height++;
         }
-        if(node.val == y){
-            yDepth = height;
-            yParent = parent;
-        }
-        parent = node;
         
-        getHeightParent(node.left, x, y, parent, height+1);
-        getHeightParent(node.right, x, y, parent, height+1);
-        
+        return xHeight == yHeight;
     }
 }
