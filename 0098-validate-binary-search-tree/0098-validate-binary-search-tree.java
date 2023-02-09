40d3a87f4d918e3fe8ef981546e2b4f3ab46de3a
Krish handelwal <78268122+krishkhandlwl@users.noreply.github.com>
2023-02-10 01:23:41 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (34.00%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -14,26 +14,21 @@
  * }
  */
 class Solution {
+    private boolean flag = true;
+    TreeNode prev = null;
     public boolean isValidBST(TreeNode root) {
-        List<Integer> list = new ArrayList<>();
-        inorder(root, list);
-        
-        Set<Integer> set = new HashSet<Integer>(list);
-
-        if(set.size() < list.size()){
-        return false;
-        }
-        
-        
-        
-        List<Integer> temp = new ArrayList<>(list);
-        Collections.sort(temp);
-        return list.equals(temp);
+        inorder(root);
+        return flag;
     }
-    public void inorder(TreeNode node, List<Integer> list){
-        if(node == null) return;
-        inorder(node.left, list);
-        list.add(node.val);
-        inorder(node.right, list);
-    }
+   public void inorder(TreeNode node){
+       if(node == null) return;
+       inorder(node.left);
+       if(prev!= null && node.val <= prev.val){
+           flag = false;
+           return;
+       }
+       prev = node;
+       inorder(node.right);
+       
+   }
 }
