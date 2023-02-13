5ae97bf713dfab9e33a341d947d44a721f472a0e
Krish handelwal <78268122+krishkhandlwl@users.noreply.github.com>
2023-02-14 03:00:47 +0530

    Time: 6 ms (61.88%), Space: 43.6 MB (62.11%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -27,3 +27,28 @@ class Solution {
         }
     }
 }
+/*
+class Solution {
+    
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        ArrayList<TreeNode> p1 = new ArrayList<>();
+        ArrayList<TreeNode> p2 = new ArrayList<>();
+        path(root, p, p1);
+        path(root, q, p2);
+        int i = 0;
+        while(i<p1.size() && i<p2.size() && p1.get(i) == p2.get(i)){
+            i++;
+        }
+        return p1.get(i-1);
+    }
+    public boolean path(TreeNode root, TreeNode node, ArrayList<TreeNode> list){
+        if(root == null) return false;
+        list.add(root);
+        if(root == node) return true;
+        if(path(root.left, node, list) || path(root.right, node, list)){
+            return true;
+        }
+        list.remove(list.size()-1);//Taaki faltu chars na store ho :P
+        return false;
+    }   
+}*/
